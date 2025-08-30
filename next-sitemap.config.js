fd46d7e88e42d940cf14a0f3e2de1f97105432ec
vietlucoder <vietleruoi@gmail.com>
2025-08-31 00:14:52 +0700

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://job-website-olive.vercel.app", // domain của bạn
+  generateRobotsTxt: true, // tự tạo robots.txt
+  sitemapSize: 5000,
+};
